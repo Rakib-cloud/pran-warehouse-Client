7f3783ed601c68186a5617fe173206b76dce7733
Rakib-cloud <ri1641715@gmail.com>
2022-05-29 03:38:04 +0600

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import Login from "./Pages/Authentication/Login/Login";
+import RequireAuth from "./Pages/Authentication/RequireAuth/RequireAuth";
+import SignUp from "./Pages/Authentication/SignUp/SignUp";
+import Blogs from "./Pages/Blogs/Blogs";
+import Home from "./Pages/Home/Home/Home";
+import AddItem from "./Pages/Inventory/AddItem/AddItem";
+import Inventory from "./Pages/Inventory/Inventory/Inventory";
+import ItemDetail from "./Pages/Inventory/ItemDetail/ItemDetail";
+import ManageItem from "./Pages/Inventory/ManageItem/ManageItem";
+import MyItems from "./Pages/Inventory/MyItems/MyItems";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
+import NotFound from "./Pages/Shared/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/inventory"
+          element={
+            <RequireAuth>
+              <Inventory />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/inventory/:itemId"
+          element={
+            <RequireAuth>
+              <ItemDetail />
+            </RequireAuth>
+          }
+        />
+        <Route path="/addItem" element={<AddItem />} />
+        <Route
+          path="/manageInventory"
+          element={
+            <RequireAuth>
+              <ManageItem />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/myItems"
+          element={
+            <RequireAuth>
+              <MyItems />
+            </RequireAuth>
+          }
+        />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer></Footer>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
